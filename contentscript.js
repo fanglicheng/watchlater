df35d0243b8175557f132ddfc52db56938a54c43
Licheng Fang <fanglicheng@gmail.com>
2014-06-28 09:44:57 -0700

    Overlay button

--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,42 @@
+$(document).ready(function() {
+        function addButton() {
+            console.log('add button');
+            var square_side = Math.min($(this).width(), $(this).height());
+            var button_side = Math.floor(square_side );
+            var button_margin = Math.floor(square_side * 0.05);
+            var button_radius = Math.floor(square_side * 0.04);
+            var button_border = Math.floor(square_side * 0.01);
+            this.button = $("<div class='overlay' </div>").css({
+                          'z-index': 3000,
+                          'position': 'absolute',
+                          'background-color': '#20d420',
+                          'opacity': 0.5,
+                          'border': '2px solid white',
+                          'border-radius': button_radius,
+                          'top': '0',
+                          'left': '0',
+                          'width': button_side,
+                          'height': button_side
+                          });
+            this.button.hover(function () {
+                console.log('hover');
+            });
+            this.button.appendTo($(this));
+            $(this).mouseenter(
+                    function () {
+                        console.log('mouseenter');
+                        $(this.button).fadeIn(100);
+                    });
+            $(this).mouseleave(
+                    function () {
+                        console.log('mouseleave');
+                        $(this.button).fadeOut(100);
+                    });
+        }
 
-function getAllElementsWithAttribute(attribute)
-{
-  var matchingElements = [];
-  var allElements = document.getElementsByTagName('*');
-  for (var i = 0, n = allElements.length; i < n; i++)
-  {
-    if (allElements[i].getAttribute(attribute))
-    {
-      // Element exists with attribute. Add to array.
-      matchingElements.push(allElements[i]);
-    }
-  }
-  return matchingElements;
-}
+        $('.html5-video-container').each(addButton);
+        $('img').each(addButton);
+});
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
